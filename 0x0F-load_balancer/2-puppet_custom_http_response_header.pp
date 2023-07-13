1a0d5ffc17f77b5f2eefc298c723f739782668d6
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-13 20:16:02 +0300

    [Update]: Update existing code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,8 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => 'present',
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-}
-
-# Define the custom HTTP header
-$custom_header_name = 'X-Served-By'
-$custom_header_value = $::hostname
-
-# Create a template file for the custom header configuration
-file { '/etc/nginx/custom_http_header.conf':
-  content => "add_header ${custom_header_name} \"${custom_header_value}\";",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Include the custom header configuration file in Nginx configuration
-file_line { 'include_custom_http_header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => "include /etc/nginx/custom_http_header.conf;",
-  match => '^http {',
-}
-
-# Restart Nginx if the configuration changes
-exec { 'nginx_reload':
-  command => '/usr/sbin/service nginx reload',
-  refreshonly => true,
+# creates a custom HTTP header response
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
 }
